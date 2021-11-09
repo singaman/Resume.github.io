64ed77b253abbcb1377e76fbefc738da58a35a68
singaman <singhaman2750@gmail.com>
2021-11-09 11:08:05 +0530

    Web
    Resume

---
+++ New folder/Auth.js
@@ -0,0 +1,14 @@
+function validate()
+{
+ var username =document.getElementById('use').className = "un";;
+ var password = document.getElementById('paso').className = "pass";;
+if(username =="Aman Singh" && password=="AmanSingh")
+  {
+  alert("Login Sucessfull");
+  return false; 
+  }
+else
+   {
+        alert("Logoin Sucessfull");
+    }
+}
